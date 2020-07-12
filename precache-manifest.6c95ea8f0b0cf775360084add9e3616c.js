2fddbfaec3aa8c32afa4d230c34aeab54d9c93fd
David Curran <dcurran@kisticket.com>
2020-07-12 13:40:28 -0500

    Updates

---
+++ precache-manifest.6c95ea8f0b0cf775360084add9e3616c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac8b2eba2078ab45b61a494f616d626",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7ee1a1aedd590ee796ba",
+    "url": "/monsters-rolodex/static/css/main.3b471823.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee1a1aedd590ee796ba",
+    "url": "/monsters-rolodex/static/js/main.abc3457f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
